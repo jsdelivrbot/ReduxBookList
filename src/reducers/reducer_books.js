6b75b58faf160ae8fb5eaf2954a8c606b3ccc3d0
swaption2009 <swaption2009@gmail.com>
2017-09-07 00:18:28 -0700

    book-list action creator
    - add an action function inside actions folder
    - import bindActionCreators
    - export mapDispatchToProps function

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -3,6 +3,7 @@ export default function() {
     { title: "Javascript: The Good Parts", pages: 101 },
     { title: "Harry Potter", pages: 39 },
     { title: "The Dark Tower", pages: 85 },
-    { title: "Eloquent Ruby", pages: 1 }
+    { title: "Eloquent Ruby", pages: 1 },
+    { title: "Guide to Galaxy", pages: 15 }
   ];
 }
